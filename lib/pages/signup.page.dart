137a06f0da508da66c36f79f228489ddacd058a5
joaofranciscopm <78879881+joaofranciscopm@users.noreply.github.com>
2022-11-07 16:48:47 -0300

    Ajustei umas coisas erradas
    Alterei o tipó de button

--- lib/pages/signup.page.dart
+++ lib/pages/signup.page.dart
@@ -5,7 +5,7 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 10, left: 40, right:40),
+        padding: EdgeInsets.only(top: 10, left: 40, right: 40),
         color: Colors.white,
         child: ListView(
           children: <Widget>[
@@ -26,8 +26,8 @@ class SignupPage extends StatelessWidget {
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topLeft,
-                    end:Alignment.bottomRight,
-                    stops: [0.3,1.0],
+                    end: Alignment.bottomRight,
+                    stops: [0.3, 1.0],
                     colors: [
                       Color(0xFFF58524),
                       Color(0xFFF92B7F),
@@ -39,121 +39,121 @@ class SignupPage extends StatelessWidget {
                   ),
                   borderRadius: BorderRadius.all(
                     Radius.circular(56),
-                    ),
+                  ),
                 ),
                 child: SizedBox.expand(
-                  child:FlatButton(
-                    child:Icon(
+                  child: TextButton(
+                    child: Icon(
                       Icons.add,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {},
-                    ) ,
-                    ) ,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
                 labelText: "Nome",
                 labelStyle: TextStyle(
-                  color:Colors.black38,
+                  color: Colors.black38,
                   fontWeight: FontWeight.w400,
                   fontSize: 20,
                 ),
-                ),
-                style: TextStyle(
-                  fontSize:20, 
-                  ),
               ),
-              SizedBox(
-                height: 10,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText:"E-mail",
-                    labelStyle:TextStyle(
-                      color:Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize:20,
-                       ),
-                        ),
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
                 ),
-                SizedBox(
-                  height:10,
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0xFFF58524),
+                    Color(0xFFF92B7F),
+                  ],
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Senha",
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize:20,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: TextButton(
+                  child: Text(
+                    "Cadastrar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
+                    textAlign: TextAlign.center,
                   ),
-                  style: TextStyle(fontSize: 20),
+                  onPressed: () {},
                 ),
-                SizedBox(
-                  height:10,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.center,
+              child: TextButton(
+                child: Text(
+                  "Cancelar",
+                  textAlign: TextAlign.center,
                 ),
-                Container(
-                  height: 60,
-                  alignment: Alignment.centerLeft,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      stops: [0.3,1],
-                      colors: [
-                        Color(0xFFF58524),
-                        Color(0xFFF92B7F),
-                      ],
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5),
-                      ),
-                  ),
-                  child: SizedBox.expand(
-                    child: FlatButton(
-                      child: Text(
-                        "Cadastrar",
-                        style:TextStyle(
-                          fontWeight:FontWeight.bold,
-                          color:Colors.white,
-                          fontSize: 20,
-                           ),
-                           textAlign: TextAlign.center,
-                           ),
-                           onPressed: (){},
-                           ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: FlatButton(
-                    child:Text(
-                      "Cancelar",
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: ()=> Navigator.pop(context, false),
-                    ),
-                ),
+                onPressed: () => Navigator.pop(context, false),
+              ),
+            ),
           ],
         ),
-      ), 
+      ),
     );
   }
 }
